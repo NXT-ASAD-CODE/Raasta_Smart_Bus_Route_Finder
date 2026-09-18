19e00377db0a9210551b93e5da2e42f918aba083
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-18 20:45:45 +0500

    Making the Mongo db Connection Complete

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-
+connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
