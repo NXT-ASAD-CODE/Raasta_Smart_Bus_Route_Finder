5347ffa24d17d6cbb35142a38c45b660fd4218ed
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-18 21:47:27 +0500

    Importing the Stop Routes in the Server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const cityRoutes = require("./routes/cityRoutes");
+const stopRoutes = require("./routes/stopRoutes");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/cities", cityRoutes);
+app.use("/api/stops", stopRoutes);
 
 app.get("/", (req, res) => {
     res.json({
