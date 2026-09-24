913b9eb3059b20e8c082e846a7a6a82071ba7804
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-24 20:18:30 +0500

    add admin authentication dependencies

---
+++ server/models/admin.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        mobile: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Admin", adminSchema);
